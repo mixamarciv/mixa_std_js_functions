4fd54a38aabde3e62e4f379cc95df5649f951a39
mixa marciv <mixamarciv@gmail.com>
2014-01-21 17:19:49 +0400

    no changes, commit for publish this files

---
+++ index.js
@@ -0,0 +1,10 @@
+
+exports.str  = require("./lib/str.js");
+exports.int  = require("./lib/int.js");
+exports.dump = require("./lib/dump.js");
+exports.path = require("./lib/path.js");
+
+
+
+exports.type = require("./lib/type.js");
+
