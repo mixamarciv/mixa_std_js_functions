a8c2dfc803dffe405665a99e801dc0771cc6f7d9
mixa marciv <mixamarciv@gmail.com>
2014-02-15 15:23:42 +0400

    update obj

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports.str  = require("./lib/str.js");
 module.exports.int  = require("./lib/int.js");
 module.exports.dump = require("./lib/dump.js");
 module.exports.path = require("./lib/path.js");
-
+module.exports.obj  = require("./lib/obj.js");
 
 
 module.exports.type = require("./lib/type.js");
