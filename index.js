8631901107c192f069f574c36bf4b42c9b4c7e30
mixa marciv <mixamarciv@gmail.com>
2015-04-18 12:13:11 +0300

    ups2

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 
 module.exports.bigint  = require("bigint-node"); //Leemon Baird's BigInt Lib
+module.exports.uuid    = require("node-uuid");
 
-module.exports.str  = require("./lib/str.js");
 module.exports.int  = require("./lib/int.js");
 module.exports._int = require("./lib/int.js");
+
+module.exports.uuid_str = require("./lib/uuid_str.js");
+
+module.exports.str  = require("./lib/str.js");
+
 module.exports.dump = require("./lib/dump.js");
 module.exports.path = require("./lib/path.js");
 module.exports.obj  = require("./lib/obj.js");
 
 
-
 module.exports.type = require("./lib/type.js");
 module.exports.ip   = require("./lib/ip.js");
+
 
 
